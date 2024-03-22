e4ebf8a957f20f6f281782a890dd2585bfd12d6b
lololibc <160233179+lololibc@users.noreply.github.com>
2024-03-22 11:58:18 +0000

    comit do gui chato

---
+++ cliente/js/sala.js
@@ -0,0 +1,27 @@
+export default class sala extends Phaser.Scene {
+  constructor () {
+    super('sala')
+  }
+
+  preload () {
+    this.load.audio('iniciar', './assets/iniciar.mp3')
+
+    this.load.image('fundo', './assets/fundo.png')
+  }
+
+  create () {
+    this.iniciar = this.sound.add('iniciar')
+
+    this.add.image(400, 225, 'fundo')
+
+    this.add.text(100, 50, 'Sala 1')
+    .setInteractive()
+    .on('pointerdown', () => {
+        this.iniciar.play ()
+    } )
+  }
+
+  update () {
+
+  }
+}
